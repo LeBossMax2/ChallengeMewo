84005181e4ed7b46d6b0984996b19de6e1301dac
Baptiste B <baptistebenard.76430@gmail.com>
2021-10-22 12:05:00 +0200

    Fix issues

--- main.py
+++ main.py
@@ -1,31 +1,31 @@
 import numpy as np
 import pandas as pd
-import sklearn
+import sklearn.model_selection
+import tensorflow as tf
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Dropout, Flatten
 from custom_metric import custom_metric_function
 
-x_data = pd.read_csv("../train_X.csv", index_col=0, sep=',')
-y_data = pd.read_csv("../train_y.csv", index_col=0, sep=',')
+x_data = pd.read_csv("../train_X.csv", index_col=0, sep=',')[0:100]
+y_data = pd.read_csv("../train_y.csv", index_col=0, sep=',')[0:100]
 
 x_train, x_valid, y_train, y_valid = sklearn.model_selection.train_test_split(x_data, y_data)
 
-x_valid = pd.read_csv("../train_X.csv", index_col=0, sep=',')
-y_valid = pd.read_csv("../train_y.csv", index_col=0, sep=',')
-
-x_test = pd.read_csv("../test_X.csv", index_col=0, sep=',')
+x_test = pd.read_csv("../test_X.csv", index_col=0, sep=',')[0:100]
 
 model = Sequential()
+
+custom_loss = tf.keras.losses.MeanSquaredError()
 
 model.add(Dense(100, activation="relu", input_dim = x_train.shape[1]))
 model.add(Dense(100, activation="relu"))
 model.add(Dense(248, activation="sigmoid"))
-model.compile(loss=custom_metric_function, optimizer="adam", metrics="mae")
+model.compile(loss=custom_loss, optimizer="adam", metrics="mae")
 
 model.summary()
 
-batch_size = 128
-epochs = 12
+batch_size = 10
+epochs = 1
 
 model.fit(x_train, y_train,
           batch_size=batch_size,
@@ -36,3 +36,8 @@ model.fit(x_train, y_train,
 score = model.evaluate(x_valid, y_valid, verbose=0)
 print('Test loss:', score[0])
 print('Test accuracy:', score[1])
+
+res = model.predict(x_test)
+res = tf.greater(res, tf.constant([0.5]))
+res_df = pd.DataFrame(res, columns=y_data.columns, dtype=int)
+res_df.to_csv("../pred_y.csv")
