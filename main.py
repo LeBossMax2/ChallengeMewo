1e0bd0fe2f2c67e4161ee7e0e1ea4620f5faf021
Baptiste B <baptistebenard.76430@gmail.com>
2021-10-22 15:15:04 +0200

    Use whole dataset

--- main.py
+++ main.py
@@ -6,16 +6,16 @@ from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Dropout, Flatten
 from custom_metric import custom_metric_function
 
-x_data = pd.read_csv("../train_X.csv", index_col=0, sep=',')[0:100]
-y_data = pd.read_csv("../train_y.csv", index_col=0, sep=',')[0:100]
+x_data = pd.read_csv("../train_X.csv", index_col=0, sep=',')
+y_data = pd.read_csv("../train_y.csv", index_col=0, sep=',')
 
 x_train, x_valid, y_train, y_valid = sklearn.model_selection.train_test_split(x_data, y_data)
 
-x_test = pd.read_csv("../test_X.csv", index_col=0, sep=',')[0:100]
+x_test = pd.read_csv("../test_X.csv", index_col=0, sep=',')
 
 model = Sequential()
 
-custom_loss = tf.keras.losses.MeanSquaredError()
+custom_loss = tf.keras.losses.BinaryCrossentropy()
 
 model.add(Dense(100, activation="relu", input_dim = x_train.shape[1]))
 model.add(Dense(100, activation="relu"))
@@ -25,7 +25,7 @@ model.compile(loss=custom_loss, optimizer="adam", metrics="mae")
 model.summary()
 
 batch_size = 10
-epochs = 1
+epochs = 30
 
 model.fit(x_train, y_train,
           batch_size=batch_size,
@@ -39,5 +39,5 @@ print('Test accuracy:', score[1])
 
 res = model.predict(x_test)
 res = tf.greater(res, tf.constant([0.5]))
-res_df = pd.DataFrame(res, columns=y_data.columns, dtype=int)
+res_df = pd.DataFrame(res, columns=y_data.columns).astype(int)
 res_df.to_csv("../pred_y.csv")
